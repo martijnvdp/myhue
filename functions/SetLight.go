064c394c5866783504b119aa7725376ef7a5320c
martijn van der ploeg <martijn.ploeg@gmail.com>
2021-01-04 19:33:03 +0100

    brightness

--- functions/SetLight.go
+++ functions/SetLight.go
@@ -5,11 +5,13 @@ import (
 )
 
 //GetAll get lights from hue bridge
-func SetLight(n *int, a *bool, token string, b *huego.Bridge) (err string) {
+func SetLight(n *int, a *bool, br *int, token string, b *huego.Bridge) (err string) {
 
 	light, _ := b.GetLight(*n)
 	if *a {
 		light.On()
+		brl := uint8(*br)
+		light.Bri(brl)
 	} else {
 		light.Off()
 	}
--- main.go
+++ main.go
@@ -9,7 +9,8 @@ import (
 )
 
 func main() {
-	l := flag.Int("light", 0, "an int")
+	l := flag.Int("light", 0, "Light ID")
+	br := flag.Int("bright", 77, "brightness level 1-254")
 	list := flag.Bool("list", false, "list all lights")
 	a := flag.Bool("on", false, "a bool")
 	flag.Parse()
@@ -21,6 +22,6 @@ func main() {
 		functions.ListAll(token, bridge)
 	}
 	if *l != 0 {
-		functions.SetLight(l, a, token, bridge)
+		functions.SetLight(l, a, br, token, bridge)
 	}
 }
