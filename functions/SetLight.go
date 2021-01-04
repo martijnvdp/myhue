a45e71e7572527b131fb5bc7200933044bbbd958
martijn van der ploeg <martijn.ploeg@gmail.com>
2021-01-04 18:14:15 +0100

    add arguments

--- functions/SetLight.go
+++ functions/SetLight.go
@@ -5,10 +5,10 @@ import (
 )
 
 //GetAll get lights from hue bridge
-func SetLight(n int, a bool, token string, b *huego.Bridge) (err string) {
+func SetLight(n *int, a *bool, token string, b *huego.Bridge) (err string) {
 
-	light, _ := b.GetLight(n)
-	if a {
+	light, _ := b.GetLight(*n)
+	if *a {
 		light.On()
 	} else {
 		light.Off()
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 
@@ -8,10 +9,14 @@ import (
 )
 
 func main() {
+	l := flag.Int("light", 1, "an int")
+	a := flag.Bool("on", false, "a bool")
+	flag.Parse()
 	token, bridge := functions.ConnectHUE()
 	if os.Getenv("HUETOKEN") == "" {
 		fmt.Println("set ENV HUETOKEN=", token)
 	}
-	functions.SetLight(1, true, token, bridge)
 	functions.ListAll(token, bridge)
+
+	functions.SetLight(l, a, token, bridge)
 }
