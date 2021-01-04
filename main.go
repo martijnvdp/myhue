d49a1505fc06720d38f95a38f8bd3eaad3a832f4
martijn van der ploeg <martijn.ploeg@gmail.com>
2021-01-04 01:08:50 +0100

    add token msg

--- main.go
+++ main.go
@@ -1,9 +1,17 @@
 package main
 
-import "github.com/martijnxd/myhue/functions"
+import (
+	"fmt"
+	"os"
+
+	"github.com/martijnxd/myhue/functions"
+)
 
 func main() {
 	token, bridge := functions.ConnectHUE()
+	if os.Getenv("HUETOKEN") == "" {
+		fmt.Println("set ENV HUETOKEN=", token)
+	}
 	functions.SetLight(1, true, token, bridge)
 	functions.ListAll(token, bridge)
 }
