1f6b668860ed5ee405af89ddfdad67f3740dbc9a
martijn van der ploeg <martijn.ploeg@gmail.com>
2021-01-04 19:39:54 +0100

    brightness

--- main.go
+++ main.go
@@ -9,15 +9,18 @@ import (
 )
 
 func main() {
+	// Arguments
 	l := flag.Int("light", 0, "Light ID")
 	br := flag.Int("bright", 77, "brightness level 1-254")
 	list := flag.Bool("list", false, "list all lights")
 	a := flag.Bool("on", false, "a bool")
 	flag.Parse()
+	// connect return token
 	token, bridge := functions.ConnectHUE()
 	if os.Getenv("HUETOKEN") == "" {
 		fmt.Println("set ENV HUETOKEN=", token)
 	}
+	// Start
 	if *list {
 		functions.ListAll(token, bridge)
 	}
