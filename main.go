6e6b329121afe16b07dfeccb99131e675feb869f
martijnxd <martijn.ploeg@gmail.com>
2021-02-07 22:55:08 +0100

    switched to cobra libary for cli cmd

--- main.go
+++ main.go
@@ -1,30 +1,22 @@
+/*
+Copyright © 2021 M van der Ploeg
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package main
 
-import (
-	"flag"
-	"fmt"
-	"os"
-
-	"github.com/martijnxd/myhue/functions"
-)
+import "github.com/martijnxd/myhue/cmd"
 
 func main() {
-	// Arguments
-	l := flag.Int("light", 0, "Light ID")
-	br := flag.Int("bright", 77, "brightness level 1-254")
-	list := flag.Bool("list", false, "list all lights")
-	a := flag.Bool("on", false, "a bool")
-	flag.Parse()
-	// connect return token
-	token, bridge := functions.ConnectHUE()
-	if os.Getenv("HUETOKEN") == "" {
-		fmt.Println("set ENV HUETOKEN=", token)
-	}
-	// Start
-	if *list {
-		functions.ListAll(token, bridge)
-	}
-	if *l != 0 {
-		functions.SetLight(l, a, br, token, bridge)
-	}
+	cmd.Execute()
 }
