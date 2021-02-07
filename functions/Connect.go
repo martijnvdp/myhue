57e36a035c28fc7871329675e282ddbc080bdc30
martijnxd <martijn.ploeg@gmail.com>
2021-02-08 00:24:30 +0100

    added set user name if not set

--- functions/Connect.go
+++ functions/Connect.go
@@ -1,9 +1,12 @@
 package functions
 
 import (
+	"bufio"
+	"fmt"
 	"os"
 
 	"github.com/amimof/huego"
+	"github.com/spf13/viper"
 )
 
 //ConnectHUE to HUE and return TOKEN
@@ -11,7 +14,11 @@ func ConnectHUE() (token string, bridge *huego.Bridge) {
 	token = os.Getenv("HUETOKEN")
 	user := os.Getenv("HUEUSER")
 	hostip := os.Getenv("HUEIP")
-
+	if user == "" {
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Print("No HUEUSER env set, enter username: ")
+		user, _ = reader.ReadString('\n')
+	}
 	if hostip == "" {
 		bridge, _ = huego.Discover()
 	} else {
@@ -19,6 +26,9 @@ func ConnectHUE() (token string, bridge *huego.Bridge) {
 	}
 	if token == "" {
 		token, _ = bridge.CreateUser(user)
+		viper.SetDefault("token", token)
+		viper.SetDefault("user", user)
+		viper.SafeWriteConfig()
 	}
 	bridge.Login(token)
 	return token, bridge
