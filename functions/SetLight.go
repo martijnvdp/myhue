95432eafb031aca137fe5ccfc4c365339b78e2df
martijnxd <martijn.ploeg@gmail.com>
2021-02-08 22:11:47 +0100

    error handling

--- functions/SetLight.go
+++ functions/SetLight.go
@@ -7,7 +7,10 @@ import (
 //SetLight state on or off and brightness level
 func SetLight(n *int, a *bool, br *int, token string, b *huego.Bridge) (err string) {
 
-	light, _ := b.GetLight(*n)
+	light, err2 := b.GetLight(*n)
+	if err2 != nil {
+		panic(err2)
+	}
 	if *a {
 		light.On()
 		brl := uint8(*br)
